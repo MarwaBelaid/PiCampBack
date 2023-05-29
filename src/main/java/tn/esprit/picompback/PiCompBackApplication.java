6a3c5e5df7c663bf3dd43a7b011ce234db03bcfa
Marwa Belaid <marwa.belaid@esprit.tn>
2023-05-29 23:01:24 +0100

    First commit Back

---
+++ src/main/java/tn/esprit/picompback/PiCompBackApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.picompback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCompBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiCompBackApplication.class, args);
+    }
+
+}
