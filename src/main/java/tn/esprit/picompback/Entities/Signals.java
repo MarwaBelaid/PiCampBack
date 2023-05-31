a2c53d9925faef8e9c93d313ac5541aa9cc0cee8
Marwa Belaid <marwa.belaid@esprit.tn>
2023-05-31 10:54:34 +0100

    update class signal 2

--- src/main/java/tn/esprit/picompback/Entities/Signals.java
+++ src/main/java/tn/esprit/picompback/Entities/Signals.java
@@ -1,2 +1,30 @@
-package tn.esprit.picompback.Entities;public class Signals {
+package tn.esprit.picompback.Entities;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Signals implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id_signal ;
+    String motif_signal;
+    Date date_signal ;
+
+
 }
